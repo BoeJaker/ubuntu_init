9c78f872156182c9c52f01d60fd63c4d642f4fa0
boejaker <j.baker.cwp@gmail.com>
2018-06-29 22:54:25 +0100

    autocommit on change

--- file_mesh_dependancies.sh
+++ file_mesh_dependancies.sh
@@ -1,4 +1,11 @@
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+# Get settings constants from the conf file 
+source $DIR/file_mesh.conf
+
 #This file Describes and installs all dependencies
+git config --global user.name "$GITHUB_USER"
+git config --global user.email "$GITHUB_EMAIL"
 
 #Rclone + config
 curl https://rclone.org/install.sh | sudo bash &
