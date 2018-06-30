de9cf4f77d9018a867198bb7ae0861a4ebd6488c
boejaker <j.baker.cwp@gmail.com>
2018-06-30 01:21:09 +0100

    autocommit on change

--- file_mesh_dependancies.sh
+++ file_mesh_dependancies.sh
@@ -5,7 +5,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 # Get settings constants from the conf file 
 source $DIR/file_mesh.conf
 
-sudo apt-get install git lib-curl  
+sudo apt-get install git curl  
 
 git config --global user.name "$GITHUB_USER"
 git config --global user.email "$GITHUB_EMAIL"
