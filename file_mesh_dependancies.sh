c1417efdc45b46c0600cf24969e2c04137b174e8
boejaker <j.baker.cwp@gmail.com>
2018-06-30 01:23:22 +0100

    autocommit on change

--- file_mesh_dependancies.sh
+++ file_mesh_dependancies.sh
@@ -13,7 +13,7 @@ git config --global credential.helper cache
 git config --global credential.helper 'cache --timeout=3600' # Set the cache to timeout after 1 hour (setting is in seconds)
 
 #Rclone + config
-if [ "$(apt list | grep '^rclone/' &>/dev/null)" ] ; then
+if [ "$(apt list 2>/dev/null | grep '^rclone/' &>/dev/null)" ] ; then
 
 	curl https://rclone.org/install.sh | sudo bash &
 	wait
