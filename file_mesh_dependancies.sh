464563d1a2a133c543acbb76fea67a4e075c018d
boejaker <j.baker.cwp@gmail.com>
2018-06-30 01:14:00 +0100

    autocommit on change

--- file_mesh_dependancies.sh
+++ file_mesh_dependancies.sh
@@ -5,10 +5,13 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 # Get settings constants from the conf file 
 source $DIR/file_mesh.conf
 
+sudo apt-get install git lib-curl  
+
 git config --global user.name "$GITHUB_USER"
 git config --global user.email "$GITHUB_EMAIL"
 git config --global credential.helper cache
 git config --global credential.helper 'cache --timeout=3600' # Set the cache to timeout after 1 hour (setting is in seconds)
+
 #Rclone + config
 curl https://rclone.org/install.sh | sudo bash &
 wait
